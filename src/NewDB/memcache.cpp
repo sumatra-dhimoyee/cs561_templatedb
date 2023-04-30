14b2c2dada0462605f3f45cc508fa47d67f3cf75
sumatra-dhimoyee <sumatradhimoyee@Sumatras-MacBook-Pro.local>
2023-04-29 22:53:02 -0400

    memcache

--- src/NewDB/memcache.cpp
+++ src/NewDB/memcache.cpp
@@ -4,9 +4,11 @@
 
 
 using namespace templatedb;
+using namespace std;
+
 
 template<typename K,typename V>
-bool MemCache<K,V> :: put(K key, std::vector<V> value){
+bool put(K key, std::vector<V> value){
 
     size_t entry_size = sizeof(K)+sizeof(std::chrono::time_point<std::chrono::system_clock>) + sizeof(bool) + sizeof(V)*value.size();
   
@@ -34,7 +36,7 @@ Entry<K,V>& getEntry(K key){
 }
 
 template<typename K,typename V>
-bool MemCache<K,V> :: updateEntry(K key, std::vector<V> value){
+bool updateEntry(K key, std::vector<V> value){
     try {
         Entry<K, V>& entry = this->get_entry(key);
         entry.value = value;
@@ -48,7 +50,7 @@ bool MemCache<K,V> :: updateEntry(K key, std::vector<V> value){
 }
 
 template<typename K,typename V>
-bool MemCache<K,V> :: deleteEntry(K key){
+bool deleteEntry(K key){
     try {
         Entry<K, V>& entry = this->get_entry(key);
         entry.tomb = true;
@@ -62,30 +64,30 @@ bool MemCache<K,V> :: deleteEntry(K key){
 }
 
 template<typename K,typename V>
-bool MemCache<K,V> :: compareEntries (Entry<K,V> i,Entry<K,V> j) { return (i.key<j.key); }
+bool compareEntries (Entry<K,V> i,Entry<K,V> j) { return (i.key<j.key); }
 
 template<typename K,typename V>
-void MemCache<K,V> :: sort(){
+void sort(){
     std::sort(this->memcache.begin(), this->memcache.end(), compareEntries);
 }
 
 template<typename K,typename V>
-vector<Entry<K,V>>& MemCache<K,V> :: getMemcache(){
+vector<Entry<K,V>>& getMemcache(){
     return this->memcache;
 }
 
 template<typename K,typename V>
-int MemCache<K,V> :: getBufferSize(){
+int getBufferSize(){
     return this->bufferSize.empty();
 }
 
 template<typename K,typename V>
-bool MemCache<K,V> :: isEmpty(){
+bool isEmpty(){
     return this->memcache.empty();
 }
 
 template<typename K,typename V>
-vector<K>& MemCache<K,V> :: buildKeyVector(){
+vector<K>& buildKeyVector(){
     vector<K> keys;
     for(Entry<K,V>& entry : this->memcache){
         keys.push_back(entry.key);
@@ -94,7 +96,7 @@ vector<K>& MemCache<K,V> :: buildKeyVector(){
 }
 
 template<typename K,typename V>
-vector<vector<V>>& MemCache<K,V> :: buildValueVector(){
+vector<vector<V>>& buildValueVector(){
     vector<vector<V>> values;
     for(Entry<K,V>& entry : this->memcache){
         values.push_back(entry.value);
@@ -103,7 +105,7 @@ vector<vector<V>>& MemCache<K,V> :: buildValueVector(){
 }
 
 template<typename K,typename V>
-vector<V>& MemCache<K,V> :: buildIntValueVector(){
+vector<V>& buildIntValueVector(){
     vector<vector<V>> values;
     for(Entry<K,V>& entry : this->memcache){
         values.push_back(entry.value[0]);
